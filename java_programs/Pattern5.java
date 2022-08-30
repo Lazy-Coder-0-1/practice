cba2611f1a541ab17faa244b6ebfd9356904d6bf
nandagopal <nandagopal1066@gmail.com>
2022-08-30 12:30:25 +0530

    Pattern 5

---
+++ java_programs/Pattern5.java
@@ -0,0 +1,22 @@
+public class Pattern5 {
+    public static void main(String[] args) {
+        int space = 4;
+        int star = 0;
+        for (int i = 1; i <= 7; i++) {
+            if (i <= 4) {
+                space--;
+                star++;
+            } else {
+                space++;
+                star--;
+            }
+            for (int j = 1; j <= space; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= star; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
